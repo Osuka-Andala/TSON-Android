4f08cb0c7625e1eb35f7d8e9d9806da29da5204d
Osuka-Andala <teresa.andala@gmail.com>
2016-07-28 16:36:52 +0300

    Added cangoback functionality

--- app/src/main/java/com/osukaandalatson/tsonapp/MainActivity.java
+++ app/src/main/java/com/osukaandalatson/tsonapp/MainActivity.java
@@ -38,6 +38,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onBackPressed() {
+        if(mywebView.canGoBack()){
+            mywebView.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
