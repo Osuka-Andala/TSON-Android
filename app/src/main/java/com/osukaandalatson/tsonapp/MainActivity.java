33cb69a929fa138f8c166a3a096e958cdfbf3297
Osuka-Andala <teresa.andala@gmail.com>
2016-07-28 17:12:52 +0300

    Removed floating button

--- app/src/main/java/com/osukaandalatson/tsonapp/MainActivity.java
+++ app/src/main/java/com/osukaandalatson/tsonapp/MainActivity.java
@@ -27,14 +27,7 @@ public class MainActivity extends AppCompatActivity {
         mywebView.loadUrl("http://thissideofnairobi.co.ke/");
         mywebView.setWebViewClient(new WebViewClient());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
